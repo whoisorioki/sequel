4c41ff7c3bb107a740f8a631104926747241c726
whoisorioki <adrianorioki@gmail.com>
2023-05-25 19:53:16 +0300

    Changes

--- SQLite/Fundamentals/script.sql
+++ SQLite/Fundamentals/script.sql
@@ -3,3 +3,5 @@ SELECT * FROM recent_grads;
 SELECT * FROM recent_grads LIMIT 6;
 
 SELECT DISTINCT Major, ShareWomen FROM recent_grads;
+
+SELECT Major, ShareWomen FROM recent_grads WHERE ShareWomen > 0.5;
