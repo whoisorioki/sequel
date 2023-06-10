169c36a2bb0ce828fdedd24c509dd484ce9bbc51
whoisorioki <adrianorioki@gmail.com>
2023-06-10 10:46:48 +0300

    Insert statements

---
+++ SQLite/adventure-sql/insert_sqlite3.sql
@@ -0,0 +1,5 @@
+INSERT INTO Package_Files VALUES('sqlite3','/usr/bin/sqldiff',1103280);
+INSERT INTO Package_Files VALUES('sqlite3','/usr/bin/sqlite3',1260976);
+INSERT INTO Package_Files VALUES('sqlite3','/usr/share/doc/sqlite3/copyright',1261);
+INSERT INTO Package_Files VALUES('sqlite3','/usr/share/man/man1/sqlite3.1.gz',3596);
+INSERT INTO Package_Files VALUES('sqlite3','/usr/share/doc/sqlite3/changelog.Debian.gz',35);
